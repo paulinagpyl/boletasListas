17eea4f87f3d44c723d90b55fdbd08a3bfd7fdbc
Paulina <paulinagpyl@gmail.com>
2025-05-06 12:13:33 -0400

    ejercicios documentados

--- 1_v2.py
+++ 1_v2.py
@@ -1,8 +1,10 @@
 # 5 10000 20000 30000 20000 20000
 suma =0
+#esto es un ciclo
 for i in range(int(input("¿Cuántas boletas ingresará al sistema?: "))):
     valor=int(input(f"Ingrese valor boleta {i+1}:"))
     suma+=valor 
+#terminó el ciclo
 print(f"La suma total de la boleta es {suma}")
 print(f"El iva ingresado es {0.19 *suma}")
 print(f"El monto ingresado al negocio es {0.81*suma}")
